b28bac5065513a07dfb64b5556eb3213f50285ea
melnicmihail <mihailmelnic17@gmail.com>
2023-08-09 12:24:58 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+﻿// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String productName = "mustar";
+        double price = 2.77;
+        boolean isAvailable = true;
+        char nutriScore = 'A';
+        boolean isBio = false;
+
+        String productName2 = "mustar2";
+        double price2 = 65;
+        boolean isAvailable2 = false;
+        char nutriScore2 = 'A';
+        boolean isBio2  = true;
+
+        double totalPrice = price + price2;
+        double mediumPrice = totalPrice/2;
+        System.out.println(totalPrice);
+        System.out.println(mediumPrice);
+    }
+}
+
